b76b11a14616451c8f4c7e9f103f43fc0930e883
davidfq <davidfq@gmail.com>
2024-04-25 20:20:02 +0200

    Fix SAS token expiry time

--- test/upload-blob.sh
+++ test/upload-blob.sh
@@ -10,8 +10,6 @@ RESOURCE_GROUP=$5
 STORAGE_ACCOUNT=$6
 CONTAINER_NAME=$7
 
-TEST_RUN_DATE=`date +"%Y-%m-%dT%H:%M:%SZ"`
-
 # get federated token
 GCP_TOKEN=`gcloud auth print-identity-token --impersonate-service-account=${EXAMPLE_TENANT_SA_MAIL} --audiences=api://AzureADTokenExchange`
 echo ${GCP_TOKEN}
@@ -26,15 +24,19 @@ az login --service-principal \
 # does not have access to the subscription by default
 az account set --subscription ${SUBSCRIPTION_ID}
 
+# 5 minutes from now should be more than enough (beware `-d` doesn't work on macOS)
+SAS_TOKEN_EXPIRATION=`date -u -d "5 minutes" "+%Y-%m-%dT%H:%M:%SZ"`
+
 SAS_TOKEN=`az storage container generate-sas \
   --account-name ${STORAGE_ACCOUNT} \
   --as-user --auth-mode login \
-  --expiry ${TEST_RUN_DATE} \
+  --expiry ${SAS_TOKEN_EXPIRATION} \
   --name ${CONTAINER_NAME} \
   --permissions rwl \
   -o tsv`
 echo "Getting SAS token: ${SAS_TOKEN}"
 
+TEST_RUN_DATE=`date +"%Y-%m-%dT%H:%M:%SZ"`
 FILENAME=test-${TEST_RUN_DATE}.txt
 echo "TEST ${TEST_RUN_DATE}" > /tmp/${FILENAME}
 echo "Copying test file to container: ${CONTAINER_NAME}"
